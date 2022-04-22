6aea70df58dd5566d2e51e731fae2b0d36874c83
pashaman <plavel@gmail.com>
2022-04-22 09:31:50 +0300

    refactore(data-sync): added clearTimeout

--- src/data-source/data-source-sync.ts
+++ src/data-source/data-source-sync.ts
@@ -11,6 +11,7 @@ import { dataSourceController } from '../controllers';
 
 export class DataSourceSync {
   private eventEmitter: EventEmitter;
+  private timeOut: NodeJS.Timeout
 
   constructor() {
     this.eventEmitter = new EventEmitter();
@@ -24,19 +25,22 @@ export class DataSourceSync {
       } catch (e) {
         console.log(`Can\'t synchronize with The Movie DB: ${e.message}`)
       }
-      setTimeout(() => {
+      this.timeOut = setTimeout(() => {
         this.eventEmitter.emit(SYNC_EVENT_NAME);
       }, SYNC_TIMEOUT)
     })
   }
 
   public start() {
-    setTimeout(() => {
+    this.timeOut = setTimeout(() => {
       this.eventEmitter.emit(SYNC_EVENT_NAME);
     }, SYNC_TIMEOUT)
   }
 
   public stop() {
+    if ( this.timeOut ) {
+      clearTimeout(this.timeOut);
+    }
     this.eventEmitter.removeAllListeners(SYNC_EVENT_NAME);
   }
 
