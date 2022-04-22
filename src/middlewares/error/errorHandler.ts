60ecf378f31aeea15d841e287cb711047f77b4fd
pashaman <plavel@gmail.com>
2022-04-22 10:35:09 +0300

    refactore(validation): fixed following to joi updates

--- src/middlewares/error/errorHandler.ts
+++ src/middlewares/error/errorHandler.ts
@@ -7,12 +7,7 @@ export function errorHandler(
   res: Response,
   next: NextFunction
 ) {
-  if (error.error && error.error.isJoi) {
-    return res.status(EHttpStatus.BAD_REQUEST).json({
-      message: error.error.message,
-    });
-  }
-  return res.status(EHttpStatus.SERVER_ERROR).json({
+  return res.status(EHttpStatus.BAD_REQUEST).json({
     message: error.message,
   });
 }
